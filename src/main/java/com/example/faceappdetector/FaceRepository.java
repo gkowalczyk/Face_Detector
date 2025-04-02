4449df69ac90435a7bb4c3c504e1a9e6cc691f2a
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-04-02 16:50:40 +0200

    modify Class on WebClient

--- src/main/java/com/example/faceappdetector/FaceRepository.java
+++ src/main/java/com/example/faceappdetector/FaceRepository.java
@@ -1,7 +1,7 @@
 package com.example.faceappdetector;
 
 import com.example.faceappdetector.model.FaceObject;
-import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface FaceRepository extends MongoRepository<FaceObject, String> {
-}
+//public interface FaceRepository extends MongoRepository<FaceObject, String> {
+//}
