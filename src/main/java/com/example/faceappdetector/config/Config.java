e724e4c1e2a604138f64afd7f8b0ec27cbbede91
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-04-08 20:16:01 +0200

    add saving data to MongoDB and FaceAttributeRequest for filtering

--- src/main/java/com/example/faceappdetector/config/Config.java
+++ src/main/java/com/example/faceappdetector/config/Config.java
@@ -1,4 +1,4 @@
-package com.example.faceappdetector;
+package com.example.faceappdetector.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
