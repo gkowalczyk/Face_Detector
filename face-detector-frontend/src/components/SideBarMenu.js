d0d173ebca99e2efab869f1b27129d8a2ffd67b6
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-04-09 19:29:42 +0200

    add saving data to MongoDB and FaceAttributeRequest for filtering + modification of frontend view

---
+++ face-detector-frontend/src/components/SideBarMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SideBarMenu = ({ onMenuItemClick }) => {
+    return (
+        <div className="sidebar">
+            <h2 className="sidebar-title">MENU</h2>
+            <ul>
+                <li onClick={() => onMenuItemClick("analyze")}>🧠 Analiza z URL</li>
+                <li onClick={() => onMenuItemClick("filter")}>🔍 Znajdź w bazie</li>
+                <li onClick={() => onMenuItemClick("about")}>👤 O aplikacji</li>
+
+            </ul>
+        </div>
+    );
+};
+export default SideBarMenu;
