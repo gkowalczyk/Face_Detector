0ff3e5d1d440f2469c11835b75c162f95b275a92
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-05-28 11:34:58 +0200

    Make mvnw executable

---
+++ src/main/java/com/example/faceappdetector/service/VideoFrameExtractor.java
@@ -0,0 +1,12 @@
+package com.example.faceappdetector.service;
+
+import lombok.RequiredArgsConstructor;
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFprobe;
+import org.springframework.stereotype.Service;
+
+
+public class VideoFrameExtractor {
+
+
+}
