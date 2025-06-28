d4500668614cc11eb4c0296c5fd5fb91777af8a4
Grzegorz Kowalczyk <86222743+gkowalczyk@users.noreply.github.com>
2025-06-28 17:33:06 +0200

    Fix callbackUrl with VideoId and State placeholders

--- face-detector-frontend/src/components/VideoUploadForm.js
+++ face-detector-frontend/src/components/VideoUploadForm.js
@@ -10,7 +10,7 @@ const VideoUploadForm = ({onStart}) => {
 
         try {
             //const response = await fetch('http://localhost:8081/api/video/add?videoUrl=' + encodeURIComponent(videoUrl), {
-                const response = await fetch('https://facedetector-production-71e7.up.railway.app/api/video/add?videoUrl='  + encodeURIComponent(videoUrl), {
+                const response = await fetch('https://face-detector-app.site/api/video/add?videoUrl='  + encodeURIComponent(videoUrl), {
                 method: 'POST'
             });
             const data = await response.json();
